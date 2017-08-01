a2940494e305b54e0f34e5b2880d551c05c9b713
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2017-08-01 13:34:36 -0500

    Quick filter --ahck

---
+++ Misc/MCC8TestFilter.h
@@ -0,0 +1,66 @@
+/**
+ * \file MCC8TestFilter.h
+ *
+ * \ingroup Misc
+ * 
+ * \brief Class def header for a class MCC8TestFilter
+ *
+ * @author ah673
+ */
+
+/** \addtogroup Misc
+
+    @{*/
+
+#ifndef LARLITE_MCC8TESTFILTER_H
+#define LARLITE_MCC8TESTFILTER_H
+
+#include "Analysis/ana_base.h"
+
+namespace larlite {
+  /**
+     \class MCC8TestFilter
+     User custom analysis class made by SHELL_USER_NAME
+   */
+  class MCC8TestFilter : public ana_base{
+  
+  public:
+
+    /// Default constructor
+    MCC8TestFilter(){ _name="MCC8TestFilter"; _fout=0;}
+
+    /// Default destructor
+    virtual ~MCC8TestFilter(){}
+
+    /** IMPLEMENT in MCC8TestFilter.cc!
+        Initialization method to be called before the analysis event loop.
+    */ 
+    virtual bool initialize();
+
+    /** IMPLEMENT in MCC8TestFilter.cc! 
+        Analyze a data event-by-event  
+    */
+    virtual bool analyze(storage_manager* storage);
+
+    /** IMPLEMENT in MCC8TestFilter.cc! 
+        Finalize method to be called after all events processed.
+    */
+    virtual bool finalize();
+
+  protected:
+
+  std::map<int,std::pair<int,int>> _evt_m ;
+    
+  };
+}
+#endif
+
+//**************************************************************************
+// 
+// For Analysis framework documentation, read Manual.pdf here:
+//
+// http://microboone-docdb.fnal.gov:8080/cgi-bin/ShowDocument?docid=3183
+//
+//**************************************************************************
+
+/** @} */ // end of doxygen group 
