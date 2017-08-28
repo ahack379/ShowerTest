b7f6cfba0dd6222ee50c6afb2f678172586c9e15
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2017-08-28 11:18:58 -0500

    Adding pi0 acript --ahack

---
+++ FindCutValue/mac/pi0_justify.py
@@ -0,0 +1,36 @@
+import sys
+
+if len(sys.argv) < 2:
+    msg  = '\n'
+    msg += "Usage 1: %s $INPUT_ROOT_FILE(s)\n" % sys.argv[0]
+    msg += '\n'
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+from larlite import larlite as fmwk
+
+my_proc = fmwk.ana_processor()
+
+for x in xrange(len(sys.argv)-1):
+    my_proc.add_input_file(sys.argv[x+1])
+
+my_proc.set_io_mode(fmwk.storage_manager.kREAD)
+
+my_proc.set_ana_output_file("cutjustify_pi0.root") 
+
+ana = fmwk.JustifyGetPi0Cuts()
+my_proc.add_process(ana)
+
+print
+print  "Finished configuring ana_processor. Start event loop!"
+print
+
+# Let's run it.
+my_proc.run()
+
+# done!
+print
+print "Finished running ana_processor event loop!"
+print
+
+sys.exit(0)
